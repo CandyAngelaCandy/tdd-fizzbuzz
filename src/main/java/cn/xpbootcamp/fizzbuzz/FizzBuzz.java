3e0a025f0c9d149f00763a73430d803a116cd5fd
Linxiao <linxiao.wei@rea-group.com>
2020-05-09 21:29:28 +0800

    [yanyan&linxiao] add implement about number contains 5 and is divisible by 5 and 7

--- src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
+++ src/main/java/cn/xpbootcamp/fizzbuzz/FizzBuzz.java
@@ -3,9 +3,11 @@ package cn.xpbootcamp.fizzbuzz;
 public class FizzBuzz {
 
     public String getNumberToFizzBuzzResult(int number) {
-        if(Integer.toString(number).contains("5") && number % 5 == 0){
+        if (Integer.toString(number).contains("5") && number % 7 == 0) {
+            return "BuzzWhizz";
+        } else if (Integer.toString(number).contains("5") && number % 5 == 0){
             return "Buzz";
-        }else if(Integer.toString(number).contains("3")){
+        }else if (Integer.toString(number).contains("3")){
             return "Fizz";
         } else if (number % 3 == 0 && number % 5 == 0 && number % 7 == 0) {
             return "FizzBuzzWhizz";
