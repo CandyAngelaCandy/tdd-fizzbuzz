f9c42b3b17e6f663c8fe49faa3d860d7a30c462a
Linxiao <linxiao.wei@rea-group.com>
2020-05-09 20:31:26 +0800

    Add test for rule 4

--- src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/cn/xpbootcamp/fizzbuzz/FizzBuzzTest.java
@@ -5,40 +5,49 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzTest {
+    @Test
+    void should_return_FizzBuzzWhizz_when_count_given_number_is_not_divisible_by_3_or_5_or_7() {
+        assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(2), "2");
+    }
 
     @Test
-    void should_return_Fizz_if_number_is_divisible_by_3(){
+    void should_return_Fizz_when_count_given_number_is_divisible_by_3(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(3),"Fizz");
     }
 
     @Test
-    void should_return_Buzz_if_number_is_divisible_by_5(){
+    void should_return_Buzz_when_count_given_number_is_divisible_by_5(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(5),"Buzz");
     }
 
     @Test
-    void should_return_Whizz_if_number_is_divisible_by_7(){
+    void should_return_Whizz_when_count_given_number_is_divisible_by_7(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(7),"Whizz");
     }
 
     @Test
-    void should_return_FizzBuzz_if_number_is_divisible_by_3_and_5(){
+    void should_return_FizzBuzz_when_count_given_number_is_divisible_by_3_and_5(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(15),"FizzBuzz");
     }
 
     @Test
-    void should_return_FizzWhizz_if_number_is_divisible_by_3_and_7(){
+    void should_return_FizzWhizz_when_count_given_number_is_divisible_by_3_and_7(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(21),"FizzWhizz");
     }
 
     @Test
-    void should_return_BuzzWhizz_if_number_is_divisible_by_5_and_7(){
+    void should_return_BuzzWhizz_when_count_given_number_is_divisible_by_5_and_7(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(35),"BuzzWhizz");
     }
 
     @Test
-    void should_return_FizzBuzzWhizz_if_number_is_divisible_by_3_and_5_and_7(){
+    void should_return_FizzBuzzWhizz_when_count_given_number_is_divisible_by_3_and_5_and_7(){
         assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(105),"FizzBuzzWhizz");
+    }
+
+    @Test
+    void should_return_Fizz_when_count_given_number_contains_3() {
+        assertEquals(new FizzBuzz().getNumberToFizzBuzzResult(13), "Fizz");
     }
 
 }
